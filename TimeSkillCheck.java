461c64a0b90866eb6719ca0204b08a59cfd6fadb
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2024-04-24 14:47:52 -0700

    Update TimeSkillCheck.java
    Added a randomTime function so user gets a time they should try to count to.
    Added a println that shows how close you were to the random time given.

--- TimeSkillCheck.java
+++ TimeSkillCheck.java
@@ -2,10 +2,13 @@
 	After the user counts to this time, they will have
 	to interact with the keyboard by clicking 'enter'
 */ 
-//import java.lang.System;
 import java.util.Scanner;
+import java.util.Random;
+import java.lang.Math;
 public class TimeSkillCheck {
 	public static void main(String[] args) {
+		int randomTime = (int) (1 + Math.random() * (5 - 1 + 1));
+		System.out.println("Count to " + randomTime " seconds");
 		// keeps time at the start of the game
 		long start = System.currentTimeMillis();
 		System.out.println(start);
@@ -16,8 +19,11 @@ public class TimeSkillCheck {
 		String userInput = stopTimer.nextLine(); // userInput doesn't matter,
 		
 		if (userInput != null) { // null so if user accidentally inputs text, the timer stops regardless
-			long end = System.currentTimeMillis();
-			System.out.println("Took : " + (end - start) + " milliseconds");
+			long end = System.currentTimeMillis(); 
+			long timeDiff = (end - start); // time it takes to click Enter 
+			System.out.println("Took : " + timeDiff + " milliseconds");
+			System.out.println("You are within " + ((randomTime * 1000) - timeDiff) + // distance from randomtime
+							  " milliseconds of randomTime");
 		}
 	}
 }
