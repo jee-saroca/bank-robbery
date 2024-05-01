629a642e04568f0b134ae2356b8d6db42e2e3753
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2024-05-01 16:51:47 -0700

    Update BankRobberyGame.java
    Added a method to delay speech whenever we need throughout the project, and added two switches for the Partner and Tool names.

--- BankRobberyGame.java
+++ BankRobberyGame.java
@@ -1,21 +1,71 @@
+import java.util.Scanner;
 public class BankRobberyGame {
-  public static void main(String[] args) {
-    Scanner charReader = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner charReader = new Scanner(System.in);
 		
-		System.out.println("DRIVER: Hey boss, where will our next score be");
-		// DelaySpeech Method
-		System.out.println("DRIVER: Oooh that's going to be tough, I think we should some help.");
-		// DelaySpeech Method
+		// INTRODUCTION
+		System.out.println("DRIVER: Hey boss, where will our next score be?");
+		delaySpeech(2); // delay is 2 seconds
+		System.out.println("DRIVER: Bank of Chase Fargo? Oooh that's going to be tough, I think we should some help.");
+		delaySpeech(3);
 		System.out.println("DRIVER: So who should we get?");
-		// DelaySpeech Method
+		delaySpeech(2);
 		
+		// PARTNER CHOICE
 		System.out.println("Type the letter of which partner you want to bring\nA. Nerd\n" +
 						  "B. Brute\nC. Sly\nD. Menace");
 		
+		String partnerName;
 		
 		char inputPartnerChoice = charReader.next().charAt(0);
-		System.out.println("You chose " + inputPartnerChoice);
+		
+		switch(Character.toUpperCase(inputPartnerChoice)) {
+			case 'A':
+				partnerName = "Nerd"; break;
+			case 'B':
+				partnerName = "Brute"; break;
+			case 'C':
+				partnerName = "Sly"; break;
+			case 'D':
+				partnerName = "Menace"; break;
+			default:
+				partnerName = "Error, this should never happen";
+		}
+		delaySpeech(1);
+		System.out.println("DRIVER: Really? You chose the... " + partnerName + "?");
+		delaySpeech(2);
+		
+		// TOOL CHOICE
+		System.out.println("Type the letter of which partner you want to bring\nA. Doohickey USB\n" +
+						  "B. Steroids\nC. Noise-Cancellation Boots\nD. Bigger Gun");
+		
+		String toolName;
+		
+		char inputToolChoice = charReader.next().charAt(0);
+		
+		switch(Character.toUpperCase(inputToolChoice)) {
+			case 'A':
+				toolName = "Doohickey USB"; break;
+			case 'B':
+				toolName = "Steroids"; break;
+			case 'C':
+				toolName = "Noise-Cancellation Boots"; break;
+			case 'D':
+				toolName = "Bigger Gun"; break;
+			default:
+				toolName = "Error, this should never happen";
+		}
+		
+		System.out.println("DRIVER: Nice choice, the " + toolName + " will be useful.");
 		charReader.close(); // not necessary for this project bc it's so small, but good practice
-    
-  }
+	}
+	
+	public static void delaySpeech(int duration) { // This needs to return int
+		try {
+			Thread.sleep(duration * 1000); // sleep uses milliseconds, so converts seconds to milli-
+		}
+		catch (Exception e) { // need this or else it causes errors
+			System.out.println(e);
+		}
+	}
 }
