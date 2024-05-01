2cc6b6a03433002d1c14ed892f9b17cc416893ea
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2024-05-01 16:15:54 -0700

    Update BankRobberyGame.java

--- BankRobberyGame.java
+++ BankRobberyGame.java
@@ -1,6 +1,21 @@
 public class BankRobberyGame {
   public static void main(String[] args) {
-    System.out.println("Hey boss, where will our next score be");
+    Scanner charReader = new Scanner(System.in);
+		
+		System.out.println("DRIVER: Hey boss, where will our next score be");
+		// DelaySpeech Method
+		System.out.println("DRIVER: Oooh that's going to be tough, I think we should some help.");
+		// DelaySpeech Method
+		System.out.println("DRIVER: So who should we get?");
+		// DelaySpeech Method
+		
+		System.out.println("Type the letter of which partner you want to bring\nA. Nerd\n" +
+						  "B. Brute\nC. Sly\nD. Menace");
+		
+		
+		char inputPartnerChoice = charReader.next().charAt(0);
+		System.out.println("You chose " + inputPartnerChoice);
+		charReader.close(); // not necessary for this project bc it's so small, but good practice
     
   }
 }
