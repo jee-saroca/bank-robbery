9c694ca154ebd84e1e4a851fccc345d0cb4ea818
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2024-05-06 21:03:31 -0700

    Uploaded ThroughVents.java part

---
+++ ThroughVents.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class BackDoorRobbery {
+	public static void main(String[] args) {
+		Scanner charReader = new Scanner(System.in);
+		// STATS
+		int intellect = 0;
+		int strength = 0;
+		int sneak = 0;
+		int intimidate = 0;
+		
+		// strings
+		String partnerName = "PARTNER";
+		String toolName = "TOOL"; 
+		
+		System.out.println("You chose to go up the vent.");
+		
+		System.out.println("PARTNER: alright! shhh.");
+		//DelaySpeech
+        System.out.println("PARTNER: in here.");
+        //*Sneak skill check*
+	}
+}
