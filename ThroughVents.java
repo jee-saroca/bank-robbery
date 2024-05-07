0497ca2f2b67caf4329e3fa58de77477a7cbcd0e
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2024-05-07 11:13:49 -0700

    Update ThroughVents.java
    Finished ThroughVents

--- ThroughVents.java
+++ ThroughVents.java
@@ -7,16 +7,43 @@ public class BackDoorRobbery {
 		int strength = 0;
 		int sneak = 0;
 		int intimidate = 0;
+		int money = 0;
 		
 		// strings
 		String partnerName = "PARTNER";
 		String toolName = "TOOL"; 
 		
+		money -= (money * 0.01);
+		
 		System.out.println("You chose to go up the vent.");
 		
+		
 		System.out.println("PARTNER: alright! shhh.");
-		//DelaySpeech
+		//delaySpeech(2);
         System.out.println("PARTNER: in here.");
-        //*Sneak skill check*
+        //skillCheckCalculator(sneak,0.20)
+		
+		System.out.println("PARTNER: let's get to the roof.");
+		//delaySpeech(2);
+        System.out.println("Before you guys make it to the roof, you hear");
+		//delaySpeech(2);
+        System.out.println("GUARD: HEY! SOMEONE BROKE IN!");
+		//delaySpeech(2);
+        System.out.println("Your partner's eyes begin to widen.");
+		//delaySpeech(2);
+        System.out.println("PARTNER: c'mon");
+		//delaySpeech(2);
+        System.out.println("You make it to the roof.");
+		//delaySpeech(2);
+        System.out.println("PARTNER: Where do we go now?");
+		//delaySpeech(3);
+		System.out.println("You can run up front to jump off the roof into some bushes, or climb down the side.");
+		char inputEntranceChoice = charReader.next().charAt(0);
+		if (Character.toUpperCase(inputEntranceChoice) == 'A') {
+			System.out.println("You chose to jump off the front of the building");
+		} else if (Character.toUpperCase(inputEntranceChoice) == 'B') {
+			System.out.println("You chose to climb down the side of the building");
+		}
+		// This would transition to either to jump down in the front or climb down side decision
 	}
 }
