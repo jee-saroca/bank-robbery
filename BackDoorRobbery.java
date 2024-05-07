15f9e4f288703d7a88baeb775c02e2fbc7204e04
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2024-05-07 15:36:19 -0700

    Update and rename BackRobbingPhase.java to BackDoorRobbery.java

--- BackDoorRobbery.java
+++ BackDoorRobbery.java
@@ -1,5 +1,6 @@
+import java.util.Random;
 import java.util.Scanner;
-public class BackRobbingPhase {
+public class BackDoorRobbery {
 	public static void main(String[] args) {
 		Scanner charReader = new Scanner(System.in);
 		// STATS
@@ -7,22 +8,23 @@ public class BackRobbingPhase {
 		int strength = 0;
 		int sneak = 0;
 		int intimidate = 0;
+		int money = 0;
 		
 		// strings
 		String partnerName = "PARTNER";
 		String toolName = "TOOL"; 
 		
 		System.out.println("You are sneaking through the back door with partner");
-		//DelaySpeech
-		System.out.println("PARTNER: Watch out for the cameras, let me disable them");
+		//delaySpeech(4)
+		System.out.println(partnerName + ": Watch out for the cameras, let me disable them");
 		//Game
-		System.out.println("PARTNER: we cracked it. we don't " +
+		System.out.println(partnerName + ": we cracked it. we don't " +
 						   "have much time, let's go.");
-		//DelaySpeech
+		//delaySpeech(4)
 		System.out.println("Both of you make it to the vault door and your partner unlocks it.");
-		//DelaySpeech
-		System.out.println("PARTNER: grab some money and let's get out of here.");
-		//DelaySpeech
+		//delaySpeech(4)
+		System.out.println(partnerName + ": grab some money and let's get out of here.");
+		//delaySpeech(4)
 		System.out.println("You think to yourself if \nA. going up the vent \nor \nB. going back up the stairs is better?: ");
 		char inputExitChoice = charReader.next().charAt(0);
 		if (Character.toUpperCase(inputExitChoice) == 'A') {
