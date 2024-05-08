fe8e15fbd632f10e06010dd192e5cbd4cc21acba
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2024-05-07 17:14:20 -0700

    Update DelaySpeech.java
    changed it to double

--- DelaySpeech.java
+++ DelaySpeech.java
@@ -1,9 +1,9 @@
 // The goal is to make a method to delay speech to give user time to read.
 public class DelaySpeech {
-	public static void main(String[] args) { // This method needs to return an int
-		int duration = 4; // 4 seconds
+	public static void main(String[] args) { // This needs get a double
+		double duration = 4000;
 		try {
-			Thread.sleep(duration * 1000); //duration measured in milliseconds
+			Thread.sleep(duration); //duration measured in milliseconds
 		}
 		catch (Exception e) { // need this or else it causes errors
 			System.out.println(e);
