44f66c3e170262eeb8a8e37a34c791ee890c4068
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2024-05-07 18:17:56 -0700

    Update BankRobberyGame.java
    ADDED FrontDoorRobbery as a method

--- BankRobberyGame.java
+++ BankRobberyGame.java
@@ -11,8 +11,10 @@ public class BankRobberyGame {
 		// INTRODUCTION
 		System.out.println("DRIVER: Hey boss, where will our next score be?");
 		delaySpeech(2); // delay is 2 seconds
-		System.out.println("DRIVER: Bank of Chase Fargo? Oooh that's going to be tough, I think we should some help.");
-		delaySpeech(3);
+		System.out.println("DRIVER: Bank of Chase Fargo?");
+		delaySpeech(2);
+		System.out.println("DRIVER: Oooh that's going to be tough, I think we should get some help.");
+		delaySpeech((long)2.3);
 		System.out.println("DRIVER: So who should we get?");
 		delaySpeech(2);
 		
@@ -50,7 +52,11 @@ public class BankRobberyGame {
 				intellect += 1;
 				break;
 			default:
-				partnerName = "Error, this should never happen";
+				partnerName = "Nerd"; 
+				intellect += 4;
+				sneak += 2;
+				strength += 1;
+				System.out.println("You didn't choose a possible option, so you're stuck with the Nerd.");
 		}
 		delaySpeech(1);
 		System.out.println("DRIVER: Really? You chose the... " + partnerName + "?");
@@ -78,7 +84,9 @@ public class BankRobberyGame {
 				toolName = "Bigger Gun"; 
 				intimidate += 2; break;
 			default:
-				toolName = "Error, this should never happen";
+				toolName = "Steroids";
+				strength += 2;
+				System.out.println("You didn't choose a possible option, so you're stuck with Steroids.");
 		}
 		
 		System.out.println("DRIVER: Nice choice, the " + toolName + " will be useful.");
@@ -96,14 +104,14 @@ public class BankRobberyGame {
 						   + "the:\nA. Front Door\nB. Back Door");
 		char inputEntranceChoice = charReader.next().charAt(0);
 		if (Character.toUpperCase(inputEntranceChoice) == 'A') {
-			System.out.println("You chose to go through the Front Door");
+			FrontDoorRobbery(partnerName, toolName);
 		} else if (Character.toUpperCase(inputEntranceChoice) == 'B') {
 			System.out.println("You chose to go through the Back Door");
 		}
 		charReader.close(); // not necessary for this project bc it's so small, but good practice
 	}
 	
-	public static void delaySpeech(long duration) { // This needs to int
+	public static void delaySpeech(long duration) { // This needs to long
 		try {
 			Thread.sleep(duration * 1000); // sleep uses milliseconds, so converts seconds to milli-
 		}
@@ -121,5 +129,60 @@ public class BankRobberyGame {
 		System.out.println(randomNum);
 		
 		return (randomNum < successChance); // if the randomNum 0-99 is less than successChance, the skill check passes true
+	}
+	public static void FrontDoorRobbery(String partnerName, String toolName) {
+		Scanner charReader = new Scanner(System.in);
+		System.out.println("You chose to go through the Front Door");
+		delaySpeech(4);
+		System.out.println(partnerName + ": This is it, let's go!");
+		delaySpeech(4);
+		System.out.println("You bust through the door with " + partnerName);
+		delaySpeech((long)0.25);
+		System.out.println("What do you say?:\nA. EVERYONE PUT YOUR HANDS UP\nB. "
+						  + "EVERYONE PUT YOUR HANDS DOWN\nC. GET ON THE GROUND");
+		char inputEntranceChoice = '~';
+		
+		while (Character.toUpperCase(inputEntranceChoice) != 'A' ||
+			  Character.toUpperCase(inputEntranceChoice) != 'B' ||
+			  Character.toUpperCase(inputEntranceChoice) != 'C' ) {
+			inputEntranceChoice = charReader.next().charAt(0);
+			if (Character.toUpperCase(inputEntranceChoice) == 'A') {
+				System.out.println("YOU: EVERYONE PUT YOUR HANDS UP");
+				delaySpeech((long)1.5);
+				System.out.println(partnerName + ": GET ON THE GROUND NOW");
+				break;
+			} else if (Character.toUpperCase(inputEntranceChoice) == 'B') {
+				System.out.println("YOU: EVERYONE PUT YOUR HANDS DOWN");
+				delaySpeech((long)1.5);
+				System.out.println(partnerName + " looks at you weirded out.");
+				System.out.println(partnerName + "EVERYONE PUT YOUR HANDS UP!");
+				break;
+			} else if (Character.toUpperCase(inputEntranceChoice) == 'C') {
+				System.out.println("YOU: GET ON THE GROUND");
+				delaySpeech((long)1.5);
+				System.out.println(partnerName + ": EVERYONE PUT YOUR HANDS UP");
+				break;
+			}
+			System.out.println("That isn't A, B, or C");
+		}
+		System.out.println(partnerName + ": THIS IS A ROBBERY");
+		//delaySpeech(0.1)
+    	//SkillCheckCalculator(intimidate,0.20);
+    	System.out.println("Both of you go to the teller, pointing your guns.");
+		System.out.println("Do you: \nA. Yell at bank teller\nB. Break through door");
+		char inputTellerChoice = '~';
+		while (Character.toUpperCase(inputTellerChoice) != 'A' ||
+			  Character.toUpperCase(inputTellerChoice) != 'B') {
+			inputTellerChoice = charReader.next().charAt(0);
+			if (Character.toUpperCase(inputTellerChoice) == 'A') {
+				System.out.println("You chose to yell at the bank teller.");
+				break;
+			} else if (Character.toUpperCase(inputTellerChoice) == 'B') {
+				System.out.println("You chose to break through the door");  
+				break;
+			} else {
+				System.out.println("That isn't A or B");
+			}
+		}
 	}
 }
