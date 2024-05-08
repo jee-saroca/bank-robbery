71224a9ec55c74a6c81a69a394a8d33b0fe52218
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2024-05-07 18:18:13 -0700

    Update DelaySpeech.java

--- DelaySpeech.java
+++ DelaySpeech.java
@@ -1,7 +1,7 @@
 // The goal is to make a method to delay speech to give user time to read.
 public class DelaySpeech {
-	public static void main(String[] args) { // This needs get a double
-		double duration = 4000;
+	public static void main(String[] args) { // This needs get a long
+		long duration = 4000;
 		try {
 			Thread.sleep(duration); //duration measured in milliseconds
 		}
