50e92a42ad3577d09fefb1ebc41329a95217cf2e
jee-saroca <152560253+jee-saroca@users.noreply.github.com>
2024-05-13 13:15:21 -0700

    Update ThroughVents.java
    Made it so that Noise-Cancellation Boots has an effect on the story.

--- ThroughVents.java
+++ ThroughVents.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 public class ThroughVents {
 	public static void main(String[] args) {
@@ -5,7 +6,7 @@ public class ThroughVents {
 		// STATS
 		int intellect = 0;
 		int strength = 0;
-		int sneak = 0;
+		int sneak = 4;
 		int intimidate = 0;
 		int money = 0;
 		
@@ -13,37 +14,76 @@ public class ThroughVents {
 		String partnerName = "PARTNER";
 		String toolName = "TOOL"; 
 		
-		money -= (money * 0.01);
+		money -= (money * 0.02);
 		
 		System.out.println("You chose to go up the vent.");
 		
 		
-		System.out.println("PARTNER: alright! shhh.");
+		System.out.println(partnerName + ": alright! shhh.");
 		//delaySpeech(2);
-        System.out.println("PARTNER: in here.");
-        //skillCheckCalculator(sneak,0.20)
-		
-		System.out.println("PARTNER: let's get to the roof.");
+        System.out.println(partnerName + ": in here.");
 		//delaySpeech(2);
-        System.out.println("Before you guys make it to the roof, you hear");
+		System.out.println("Your follow " + partnerName +
+						  " into the vents.");
 		//delaySpeech(2);
-        System.out.println("GUARD: HEY! SOMEONE BROKE IN!");
+		System.out.println(partnerName + ": let's get to the roof.");
+		//delaySpeech(2);
+		System.out.println("GUARD: HEY! SOMEONE BROKE IN!");
 		//delaySpeech(2);
         System.out.println("Your partner's eyes begin to widen.");
 		//delaySpeech(2);
-        System.out.println("PARTNER: c'mon");
-		//delaySpeech(2);
-        System.out.println("You make it to the roof.");
-		//delaySpeech(2);
-        System.out.println("PARTNER: Where do we go now?");
-		//delaySpeech(3);
-		System.out.println("You can run up front to jump off the roof into some bushes, or climb down the side.");
-		char inputRoofChoice = charReader.next().charAt(0);
-		if (Character.toUpperCase(inputRoofChoice) == 'A') {
-			System.out.println("You chose to jump off the front of the building");
-		} else if (Character.toUpperCase(inputRoofChoice) == 'B') {
-			System.out.println("You chose to climb down the side of the building");
+		System.out.println(partnerName + " mouths shhhh to quiet you");
+		if (toolName == "Noise-Cancellation Boots") {
+			System.out.println("Fortunately you have the " + toolName +
+							  " so your steps were silent as you crawl through the vents.");
+			//delaySpeech(2);
+			System.out.println(partnerName + ": c'mon");
+			//delaySpeech(2);
+			System.out.println("You make it to the roof.");
+			//delaySpeech(2);
+			System.out.println(partnerName + ": Where do we go now?");
+			//delaySpeech(3);
+			System.out.println("You can" +
+							   "\nA. run up front to jump off the roof into some bushes," 
+							   + "\nor \nB. climb down the side.");
+			char inputRoofChoice = charReader.next().charAt(0);
+			if (Character.toUpperCase(inputRoofChoice) == 'A') {
+				System.out.println("You chose to jump off the front of the building");
+			} else if (Character.toUpperCase(inputRoofChoice) == 'B') {
+				System.out.println("You chose to climb down the side of the building");
+			}
+		} else {
+			System.out.println("As you walk through the vents, " +
+							   "you try your hardest to not make noise");
+			if (statCheckCalculator(sneak,0.14)) {
+				System.out.println("You succeeded the stat check.");
+			} else {
+				System.out.println("You failed the stat check.");
+				System.out.println("The guards start to shoot at you");
+				if (Math.random() < (sneak * 0.14)) {
+					System.out.println("You and " + partnerName +
+									  "scramble and climb up the ladder.");
+					money -= (money * 0.02);
+				} else {
+					System.out.println("You get shot and died.");
+					//delaySpeech(2);
+					System.out.println("You lost.");
+					money -= (money * 0);
+					return money;
+				}
+			}
 		}
 		// This would transition to either to jump down in the front or climb down side decision
+	}
+	public static boolean statCheckCalculator(int multiplier, double percent) {
+	double successChance = multiplier * percent; // success is based on the skill multiplier and base chance of working
+
+	System.out.println(successChance);
+
+	double randomNum = Math.random();
+
+	System.out.println(randomNum);
+
+	return (randomNum < successChance); // if the randomNum 0-99 is less than successChance, the skill check passes true
 	}
 }
